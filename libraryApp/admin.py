e38ec079f94fbac30dcb960ab521848fcdd4f9d0
Craftyawesome <craftyawesome.ak@gmail.com>
2024-03-06 13:16:05 -0600

    delete duplicate user table

--- libraryApp/admin.py
+++ libraryApp/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from .models import User, Book   #classes that are in models.py
+from .models import Book   #classes that are in models.py
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Book)
-
-class UserAdmin(admin.ModelAdmin):
-    list_display =  ['id', 'name', 'phone_number', 'is_admin']
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author_name', 'isbn', 'rent_flag', 'genre']
--- libraryApp/models.py
+++ libraryApp/models.py
@@ -2,15 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-# User Model for database 
-class User(models.Model):
-    name            = models.CharField(max_length=255)
-    phoneNum        = models.CharField(max_length=15)
-    is_admin        = models.BooleanField(default=False) 
-
-    def __str__(self):
-        return self.name
-
 # Book Model for datbase
 class Book(models.Model):
 
