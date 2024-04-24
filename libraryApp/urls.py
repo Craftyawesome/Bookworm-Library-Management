34fd9161251d4bb614260ea7ca06c174e83bede7
RyanThomas00 <ryan.thomas2015@icloud.com>
2024-04-24 11:46:27 -0500

    request
    request

--- libraryApp/urls.py
+++ libraryApp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path 
+<<<<<<< Updated upstream
 from .views import home, book_list, genre_view, getBook_Request, book_detail, book_club
+=======
+from .views import home, book_list, genre_view, getBook_Request, book_detail, add_to_cart, request_book, book_request
+>>>>>>> Stashed changes
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -12,6 +16,9 @@ urlpatterns = [
     path('book_detail/',book_detail, name='book_detail'),
     path('getBook_Request',views.getBook_Request, name='getBook_Request'),
     path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('add_to_cart/', add_to_cart, name='add_to_cart'),
+    path('request_book/', request_book, name='request_book'),
+    path('book_request/', book_request, name='book_request'),
 
 
     
